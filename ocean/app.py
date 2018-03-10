4fb45285dd1a3974da8b537c1492fa359a6ae0e7
Pinaki Mukherjee <pinakim11@gmail.com>
2018-03-10 17:47:53 +0000

    Fixed the file already in use error - windows

--- ocean/app.py
+++ ocean/app.py
@@ -1,20 +1,71 @@
-from bottle import route,run,template
-import ConfigParser
+from bottle import route,run,template,request
+from readCfg import readProperties
+from updateKey import replace_key
 
-config = ConfigParser.RawConfigParser()
+#config = ConfigParser.RawConfigParser()
 #from readCfg import readProperties
+configFile= 'prf_mission.cfg'
+
+
+def compareProperties(request):
+    data = {}
+    keys=readProperties()
+    for key, value in keys.items():
+       if value == request.forms.get(key):
+         print(key,'not changed.')
+       else:
+         print(key,'changed.')
+         data.update({key : request.forms.get(key)})
+        
+    print(data)
+    return data
+     
 
 
 @route("/")
 def index():
-    return template('viewConfig') 
+    return template('viewConfig')
+
+
+@route("/test",method = "post")
+def index():
+    return template('testConfig')
+
 
 @route("/",method="post")
-def index():
-    # Writing our configuration file to 'example.cfg'
-    with open('prf_mission.cfg', 'r+') as configfile:
-       config.set('supervisor.gps_interval', '15')          
-       config.write(configfile)
+def test():
+    gps_interval = request.forms.get('supervisor.gps_interval')
+    wake_mode = request.forms.get('supervisor.wake_mode')
+    alarm_interval = request.forms.get('supervisor.alarm_interval')
+    print(wake_mode)
+    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+    print(gps_interval)
+    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+    print(alarm_interval)
+    updatedProps = compareProperties(request)
+    updatedVals = {}
+    if (len(updatedProps.keys()) > 0):
+            
+             # Writing our configuration file 
+              with open(configFile, 'r+') as configfile:
+                 try:   
+                    for key, value in updatedProps.items():
+                       print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+                       print('updating', key)
+                       print('value', value)
+                       print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+                       #updatedVals.update({key,value})
+                       updatedVals[key]= value
+                       #replace_key(configFile,key, value)
+                 finally:
+                    if configfile is not None:
+                      configfile.close()
+
+              if(len(updatedVals) > 0):
+                 for k,v in updatedVals.items():
+                    replace_key(configFile,k,v)
+    else:
+        print('No config values updated.')
     
     return template('viewConfig') 
 
@@ -26,7 +77,5 @@ def index():
 
 run(host='localhost',port=9090)
 
-
-
-
-
+if __name__ == "__main__":
+    run(reloader=True)
