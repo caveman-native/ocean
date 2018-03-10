895daa8cc5f18ef7d31f49fbf9e1e6a0726f7fa7
Pinaki Mukherjee <pinakim11@gmail.com>
2018-03-10 17:50:44 +0000

    Fixed the windows error.

--- ocean/readCfg.py
+++ ocean/readCfg.py
@@ -3,21 +3,27 @@ from collections import OrderedDict
 def readProperties():
    separator = "="
    keys = OrderedDict()
-
+   
+  
    with open('prf_mission.cfg') as f:
+      try:
+         for line in f:
+           if separator in line:
 
-    for line in f:
-        if separator in line:
+             # Find the name and value by splitting the string
+             name, value = line.split(separator, 1)
 
-            # Find the name and value by splitting the string
-            name, value = line.split(separator, 1)
-
-            # Assign key value pair to dict
-            # strip() removes white space from the ends of strings
-            keys[name.strip()] = value.split(' #', 1)[0].strip()
+             # Assign key value pair to dict
+             # strip() removes white space from the ends of strings
+             keys[name.strip()] = value.split(' #', 1)[0].strip()
             
-
-   print(keys)        
+      finally:
+          if f is not None:
+             f.close()
+   
+   #print(keys)        
    return keys         
+        
+    
     
 readProperties()
