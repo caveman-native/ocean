9e25124d36903299f5eb6d549209ed17837859ec
Stephen O'Brien <steveobrien73@gmail.com>
2018-03-31 00:57:10 +0000

    Adding scripts.js

---
+++ ocean/static/js/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM Loaded! Plain Javascript...");
+});
+$(function(){
+  console.log("Helloworld! Good Old jQuery...");
+});
